3d740f15fbf0a04dfbe4a159b737eff40b9ba146
roy sharon <roy.sharon.4@gmail.com>
2024-06-05 06:50:51 +0000

    task 2

---
+++ user/memsize_test.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Initial memory size: %d bytes\n", memsize());
+    void *allocation = malloc(20 * 1024);
+    printf("Memory size after malloc: %d bytes\n", memsize());
+    free(allocation);
+    printf("Memory size after free: %d bytes\n", memsize());
+
+    exit(0);
+}
